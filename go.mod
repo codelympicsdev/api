f301b306d496d20be9206fa23b0cf0a8f2aaba66
Luca Casonato <lucacasonato@yahoo.com>
2019-07-07 18:55:34 +0200

    dockerfile

--- go.mod
+++ go.mod
@@ -4,18 +4,16 @@ go 1.12
 
 require (
 	github.com/Automattic/go-gravatar v0.0.0-20141029223728-825bcfa87a57
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dlclark/regexp2 v1.1.6 // indirect
 	github.com/dop251/goja v0.0.0-20190625200431-3f2f11566cd5
 	github.com/gbrlsnchs/jwt/v3 v3.0.0-beta.0
 	github.com/go-sourcemap/sourcemap v2.1.2+incompatible // indirect
-	github.com/google/uuid v1.1.1
 	github.com/gorilla/context v1.1.1
 	github.com/gorilla/mux v1.7.3
-	github.com/kirsle/configdir v0.0.0-20170128060238-e45d2f54772f // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lucacasonato/wrap v0.3.4
-	github.com/montanaflynn/stats v0.5.0 // indirect
 	github.com/pquerna/otp v1.2.0
-	github.com/spf13/cobra v0.0.5 // indirect
-	github.com/spf13/viper v1.4.0 // indirect
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 
@@ -22,44 +22,10 @@ func main() {
 	challenges.Route(v0.PathPrefix("/challenges").Subrouter())
 	attempts.Route(v0.PathPrefix("/attempts").Subrouter())
 
-	/*
-			c := &database.Challenge{
-				Name: "Add",
-				Description: `# Add two numbers
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	}
 
-		Input: two decimal numbers seperated via newline on stdin
-		Output: result of adding the two numbers as decimal on stdout`,
-				Generator: `(function() {
-		  var one = Math.random() * 10;
-		  var two = Math.random() * 10;
-		  return {
-		    Input: {
-		      Args: [],
-		      Stdin: one.toString() + '\n' + two.toString()+ '\n',
-		    },
-		    Output: {
-		      Stdout: (one + two).toString(),
-		      Stderr: '',
-		    },
-		  };
-		})()
-		`,
-				MaxLiveAttempts: 3,
-				Timeout:         2 * time.Second,
-				PublishDate:     time.Now(),
-				ResultsDate:     time.Now().Add(10 * time.Minute),
-			}
-
-			err := c.Save()
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Println(c.ID)
-			fmt.Println(challenge.Generate(c))
-	*/
-
-	fmt.Println("5d21e24bf789f169d5548652")
-
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(port, r)
 }
